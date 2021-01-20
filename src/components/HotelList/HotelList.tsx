4b64dc1f30d04b271646ad825a17b3a243df66c8
Alex Nweke <nwekealexchinedu@gmail.com>
2021-01-20 13:18:16 +0100

    checked if data is a truthy

--- src/components/HotelList/HotelList.tsx
+++ src/components/HotelList/HotelList.tsx
@@ -19,35 +19,36 @@ interface payload {
 const HotelList: React.FC = () => {
   let numOfNights: Array<number> = [];
   Array.from(Array(100), (_, i) => numOfNights.push(i + 1));
-  const data = resource.hotels.read();
+  let data = resource.hotels.read();
   return (
     <div className={cx(styles.pageContainer)}>
       <div className="page-title">
         <h5>Hotel List</h5>
       </div>
-      {data.map(
-        (item: {
-          id: string;
-          name: string;
-          hotelPerNight: string;
-          image: string;
-          subTitle: string | undefined;
-        }) => (
-          <>
-            <div className={cx(styles.container)} key={item.id}>
-              <HotelDetails
-                name={item.name}
-                subTitle={item.subTitle}
-                image={item.image}
-              />
-              <div className={cx(styles.totalContainer)}>
-                <SelectHotel options={numOfNights} />
-                <div className={styles.total}>{"00.00 €"}</div>
+      {data &&
+        data.map(
+          (item: {
+            id: string;
+            name: string;
+            hotelPerNight: string;
+            image: string;
+            subTitle: string | undefined;
+          }) => (
+            <>
+              <div className={cx(styles.container)} key={item.id}>
+                <HotelDetails
+                  name={item.name}
+                  subTitle={item.subTitle}
+                  image={item.image}
+                />
+                <div className={cx(styles.totalContainer)}>
+                  <SelectHotel options={numOfNights} />
+                  <div className={styles.total}>{"00.00 €"}</div>
+                </div>
               </div>
-            </div>
-          </>
-        )
-      )}
+            </>
+          )
+        )}
       <div className={styles.overallTotalContainer}>
         <div>
           <div className={styles.total}>{"00.00 €"}</div>
